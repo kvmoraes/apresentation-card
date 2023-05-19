5500a80a50a11c5dc6c8b247e0698f25b4ceae62
kvmoraes <karinevitoria.marinho@gmail.com>
2023-05-19 11:32:30 -0300

    refactor: delete bio and add url

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var prettyjson = require('prettyjson');
 
 var data = {
-  username: 'kvmoraes',
-  url: 'https://github.com/kvmoraes',
-  bio: `Olá! Atualmente tenho 17 anos e estou no último 
-  ano do ensino médio. Ao longo da minha trajetória, desenvolvi
-  diferentes projetos dentro da escola utilizando a tecnologia arduíno
-  e, assim, fui iniciada no mundo da programação através da linguagem
-  C++. Em 2021, comecei a estudar por conta própria sobre desenvolvimento
-  web e me apaixonei pela área. Desde então, fiz diversos miniprojetos, 
-  sempre me dedicando a aprender cada vez mais.`
+  name: 'Karine Vitória Moraes ♥',
+  github: 'kvmoraes',
+  urls: [
+    'https://www.linkedin.com/in/karine-moraes-1776801b9',
+    'https://fourway.tech'
+  ]
 };
 
 var options = {
